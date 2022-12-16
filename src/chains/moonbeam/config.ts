0d67487845261f30a236f288e595008463526faa
mckrava <mckrava2@gmail.com>
2022-12-16 22:50:07 +0200

    fix: remove debug range

--- src/chains/moonbeam/config.ts
+++ src/chains/moonbeam/config.ts
@@ -7,9 +7,9 @@ export const config: ProcessorConfig = {
     archive: 'https://moonbeam.archive.subsquid.io/graphql',
     chain: 'wss://rpc.pinknode.io/moonbeam/6e3fa591-e24f-483a-95fa-1d773f7f2be3'
   },
-  blockRange: {
-    from: 1800000
-  },
+  // blockRange: {
+  //   from: 1800000
+  // },
   intervals: new Map([
     ['holders', 1000 * 60 * 60 * 12],
     ['issuance', 1000 * 60 * 60],
